f751781d3ef7efcee9e67886610091572c373e4c
andrer54 <andrer54@gmail.com>
2022-03-29 10:51:08 -0300

    micro fix

--- Principal.java
+++ Principal.java
@@ -4,11 +4,11 @@ import com.mycompany.empacotadoraACME.Empresa;
 
 public class Principal {
     //atributos
-    private static Empresa ACME;
+    private static Empresa ANDRE;
 
     //metodos
     public static void main ( String args [ ] ) throws InterruptedException {
         //empresa (num de fitas, empregados disponiveis, num max de equipes, produtos a serem empacotados)
-        ACME = new Empresa (20,25,4,200);
+        ANDRE = new Empresa (20,25,4,200);
     }
 }
